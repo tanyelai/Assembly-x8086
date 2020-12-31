8708db3e4330e31c660b22642e9deafd50fd4581
Toygar Tanyel <44132720+Toygarr@users.noreply.github.com>
2020-12-31 20:07:38 +0300

    Update Soru22.asm

--- LinearCongruentialGeneration/Soru22.asm
+++ LinearCongruentialGeneration/Soru22.asm
@@ -1,27 +1,27 @@
 datasg	SEGMENT BYTE 'veri'
-X       DW 0   ;sonucun tutulacagi yer 
+X       	DW 0   ;sonucun tutulacagi yer 
 m		DW 8191
 a		DW 884
 c		DW 1 
-datasg	ENDS
+datasg		ENDS
 
-stacksg	SEGMENT BYTE STACK 'yigin'
+stacksg		SEGMENT BYTE STACK 'yigin'
 		DW 12 DUP(?)
-stacksg	ENDS
+stacksg		ENDS
 
-codesg	SEGMENT PARA 'kod'
+codesg		SEGMENT PARA 'kod'
 		ASSUME CS:codesg, DS:datasg, SS:stacksg
 ANA		PROC FAR
-		PUSH DS				; geri donus adreslerinin ayarlanmasi
+		PUSH DS			; geri donus adreslerinin ayarlanmasi
 		XOR AX, AX
 		PUSH AX
 		MOV AX, datasg		; data segment degerinin ayarlanmasi
 		MOV DS, AX	
 	
-		MOV AX, cs:[23] ; seed degeri / X0
+		MOV AX, cs:[23] 	; seed degeri / X0
 		JMP atla
-tekrar:	MOV AX, X
-atla:	MUL a 
+tekrar:		MOV AX, X
+atla:		MUL a 
 		ADD AX, c 
 		DIV m
 		MOV X, DX
@@ -30,5 +30,5 @@ atla:	MUL a
 	
 		RETF
 ANA		ENDP
-codesg	ENDS
+codesg		ENDS
 		END ANA
